42fbf523af97cb2d70ca893fdb4cead51153f0d4
Vitaliy Sulimenko <31705823+Braiteck@users.noreply.github.com>
2022-03-28 18:02:59 +0300

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,289 @@
+$(() => {
+	// Есть ли поддержка тач событий или это apple устройство
+	if (!is_touch_device() || !/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) $('html').addClass('custom_scroll')
+
+
+	// Ленивая загрузка
+	setTimeout(() => {
+		observer = lozad('.lozad', {
+			rootMargin: '200px 0px',
+			threshold: 0,
+			loaded: el => el.classList.add('loaded')
+		})
+
+		observer.observe()
+	}, 200)
+
+
+	// Установка ширины стандартного скроллбара
+	$(':root').css('--scroll_width', widthScroll() + 'px')
+
+
+	// Кнопка 'Вверх'
+	$('.buttonUp button').click((e) => {
+		e.preventDefault()
+
+		$('body, html').stop(false, false).animate({ scrollTop: 0 }, 1000)
+	})
+
+
+	// Маска ввода
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99', {
+		showMaskOnHover: false
+	})
+
+	$('input.date').inputmask('99.99.9999', {
+		placeholder: '__.__.____',
+		inputFormat: "dd.mm.yyyy",
+		showMaskOnHover: false
+	})
+
+	// Кастомный select
+	$('select').niceSelect()
+
+	// Фиксация label при вводе в поле
+	$('body').on('keydown', '.form .input, .form textarea', function () {
+		let _self = $(this)
+
+		setTimeout(() => {
+			_self.val().length
+				? _self.addClass('active')
+				: _self.removeClass('active')
+		})
+	})
+
+
+	// Мини всплывающие окна
+	$('.mini_modal_btn').click(function (e) {
+		e.preventDefault()
+
+		const modalId = $(this).data('modal-id')
+
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active')
+			$('.mini_modal').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		} else {
+			$('.mini_modal_btn').removeClass('active')
+			$(this).addClass('active')
+
+			$('.mini_modal').removeClass('active')
+			$(modalId).addClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'pointer')
+		}
+	})
+
+	// Закрываем всплывашку при клике за её пределами
+	$(document).click((e) => {
+		if ($(e.target).closest('.modal_cont').length === 0) {
+			$('.mini_modal, .mini_modal_btn').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		}
+	})
+
+
+	// Табы
+	var locationHash = window.location.hash
+
+	$('body').on('click', '.tabs button, .tabs .btn', function (e) {
+		if ($(this).hasClass('btn') && $(window).width() > 1023) {
+			e.preventDefault()
+		}
+
+		if (!$(this).hasClass('btn')) {
+			e.preventDefault()
+		}
+
+		if (!$(this).hasClass('active')) {
+			const $parent = $(this).closest('.tabs_container'),
+				activeTab = $(this).data('content'),
+				$activeTabContent = $(activeTab),
+				level = $(this).data('level')
+
+			$parent.find('.tabs:first button, .tabs:first .btn').removeClass('active')
+			$parent.find('.tab_content.' + level).removeClass('active')
+
+			$(this).addClass('active')
+			$activeTabContent.addClass('active')
+		}
+	})
+
+	if (locationHash && $('.tabs_container').length) {
+		const $activeTab = $('.tabs button[data-content=' + locationHash + ']'),
+			$activeTabContent = $(locationHash),
+			$parent = $activeTab.closest('.tabs_container'),
+			level = $activeTab.data('level')
+
+		$parent.find('.tabs:first button, .tabs:first .btn').removeClass('active')
+		$parent.find('.tab_content.' + level).removeClass('active')
+
+		$activeTab.addClass('active')
+		$activeTabContent.addClass('active')
+
+		$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
+	}
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+	Fancybox.defaults.template = {
+		closeButton: '<svg><use xlink:href="images/sprite.svg#ic_close"></use></svg>',
+	}
+
+	// Всплывающие окна
+	$('body').on('click', '.modal_btn', function (e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: $(this).data('content'),
+			type: 'inline'
+		}])
+	})
+
+
+	// Аккордион
+	$('body').on('click', '.accordion .accordion_item .head', function (e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.accordion_item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			$accordion.find('.accordion_item').removeClass('active')
+			$accordion.find('.data').slideUp(300)
+
+			$item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+
+	// Изменение количества товара
+	$('body').on('click', '.amount .minus', function (e) {
+		e.preventDefault()
+
+		const $parent = $(this).closest('.amount'),
+			$input = $parent.find('.input'),
+			inputVal = parseFloat($input.val()),
+			minimum = parseFloat($input.data('minimum')),
+			step = parseFloat($input.data('step')),
+			unit = $input.data('unit')
+
+		if (inputVal > minimum) $input.val(inputVal - step + unit)
+	})
+
+	$('body').on('click', '.amount .plus', function (e) {
+		e.preventDefault()
+
+		const $parent = $(this).closest('.amount'),
+			$input = $parent.find('.input'),
+			inputVal = parseFloat($input.val()),
+			maximum = parseFloat($input.data('maximum')),
+			step = parseFloat($input.data('step')),
+			unit = $input.data('unit')
+
+		if (inputVal < maximum) $input.val(inputVal + step + unit)
+	})
+
+	$('.amount .input').keydown(function () {
+		const _self = $(this),
+			maximum = parseInt(_self.data('maximum'))
+
+		setTimeout(() => {
+			if (_self.val() == '' || _self.val() == 0) _self.val(parseInt(_self.data('minimum')))
+			if (_self.val() > maximum) _self.val(maximum)
+		})
+	})
+
+
+	// Моб. версия
+	firstResize = false
+
+	if ($(window).width() < 375) {
+		$('meta[name=viewport]').attr('content', 'width=375, user-scalable=no')
+
+		firstResize = true
+	}
+
+
+	if (is_touch_device()) {
+		// Закрытие моб. меню свайпом справо на лево
+		let ts
+
+		$('body').on('touchstart', (e) => { ts = e.originalEvent.touches[0].clientX })
+
+		$('body').on('touchend', (e) => {
+			let te = e.originalEvent.changedTouches[0].clientX
+
+			if ($('body').hasClass('menu_open') && ts > te + 50) {
+				// Свайп справо на лево
+				$('body').removeClass('menu_open')
+				$('header').removeClass('show')
+				$('.overlay').fadeOut(200)
+			} else if (ts < te - 50) {
+				// Свайп слева на право
+				$('body').removeClass('menu_open')
+				$('.auth_info, .cart_info').removeClass('show')
+				$('.overlay').fadeOut(200)
+			}
+		})
+	}
+})
+
+
+
+$(window).scroll(() => {
+	// Кнопка 'Вверх'
+	$(window).scrollTop() > $(window).innerHeight()
+		? $('.buttonUp').fadeIn(300)
+		: $('.buttonUp').fadeOut(200)
+})
+
+
+
+// Вспомогательные функции
+const setHeight = (className) => {
+	let maxheight = 0
+
+	className.each(function () {
+		const elHeight = $(this).outerHeight()
+
+		if (elHeight > maxheight) maxheight = elHeight
+	})
+
+	className.outerHeight(maxheight)
+}
+
+
+const is_touch_device = () => !!('ontouchstart' in window)
+
+
+const widthScroll = () => {
+	let div = document.createElement('div')
+
+	div.style.overflowY = 'scroll'
+	div.style.width = '50px'
+	div.style.height = '50px'
+	div.style.visibility = 'hidden'
+
+	document.body.appendChild(div)
+
+	let scrollWidth = div.offsetWidth - div.clientWidth
+	document.body.removeChild(div)
+
+	return scrollWidth
+}
---
+++ js/scripts.js
@@ -0,0 +1,309 @@
+$(() => {
+	// Ширина окна для ресайза
+	WW = $(window).width()
+
+
+	// Основной слайдер на главной
+	if ($('.main_slider .swiper-container').length) {
+		new Swiper('.main_slider .swiper-container', {
+			loop: true,
+			speed: 750,
+			watchSlidesVisibility: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			}
+		})
+	}
+
+
+	// Карусель моделей
+	const modelsSliders = []
+
+	$('.models .swiper-container').each(function (i) {
+		$(this).addClass('models_s' + i)
+
+		let slides = $(this).find('.slide').length,
+			options = {
+				loop: false,
+				speed: 500,
+				simulateTouch: false,
+				allowTouchMove: true,
+				noSwiping: true,
+				spaceBetween: 20,
+				watchSlidesVisibility: true,
+				slideActiveClass: 'active',
+				slideVisibleClass: 'visible',
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					bulletActiveClass: 'active'
+				},
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev'
+				},
+				breakpoints: {
+					0: {
+						slidesPerView: 1
+					},
+					768: {
+						slidesPerView: 2
+					},
+					1024: {
+						slidesPerView: 3
+					},
+					1280: {
+						slidesPerView: 4
+					}
+				}
+			}
+
+		modelsSliders.push(new Swiper('.models_s' + i, options))
+
+		if (slides > modelsSliders[i].params.slidesPerView) {
+			options.loop = true
+			options.simulateTouch = true
+			options.allowTouchMove = true
+			options.noSwiping = false
+
+			modelsSliders[i].destroy(true, true)
+			modelsSliders[i] = new Swiper('.models_s' + i, options)
+		}
+	})
+
+
+	// Карусель товаров
+	const productsSliders = []
+
+	$('.products .swiper-container').each(function (i) {
+		$(this).addClass('products_s' + i)
+
+		let slides = $(this).find('.slide').length,
+			options = {
+				loop: false,
+				speed: 500,
+				simulateTouch: false,
+				allowTouchMove: true,
+				noSwiping: true,
+				spaceBetween: 16,
+				watchSlidesVisibility: true,
+				slideActiveClass: 'active',
+				slideVisibleClass: 'visible',
+				breakpoints: {
+					0: {
+						slidesPerView: 1
+					},
+					768: {
+						slidesPerView: 2
+					},
+					1024: {
+						slidesPerView: 3
+					},
+					1280: {
+						slidesPerView: 3
+					}
+				}
+			}
+
+		productsSliders.push(new Swiper('.products_s' + i, options))
+
+		if (slides > productsSliders[i].params.slidesPerView) {
+			options.loop = true
+			options.simulateTouch = true
+			options.allowTouchMove = true
+			options.noSwiping = false
+
+			productsSliders[i].destroy(true, true)
+			productsSliders[i] = new Swiper('.products_s' + i, options)
+		}
+	})
+
+
+	// Карусель статей
+	const articlesSliders = []
+
+	$('.articles .swiper-container').each(function (i) {
+		$(this).addClass('articles_s' + i)
+
+		let slides = $(this).find('.slide').length,
+			options = {
+				loop: false,
+				speed: 500,
+				simulateTouch: false,
+				allowTouchMove: true,
+				noSwiping: true,
+				spaceBetween: 0,
+				watchSlidesVisibility: true,
+				slideActiveClass: 'active',
+				slideVisibleClass: 'visible',
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev'
+				},
+				breakpoints: {
+					0: {
+						slidesPerView: 1
+					},
+					768: {
+						slidesPerView: 2
+					}
+				},
+				on: {
+					init: swiper => {
+						setTimeout(() => {
+							$(swiper.$el).find('.swiper-button-next, .swiper-button-prev').css(
+								'top', $(swiper.$el).find('.thumb').outerHeight() * 0.5
+							)
+						})
+					},
+					resize: swiper => {
+						setTimeout(() => {
+							$(swiper.$el).find('.swiper-button-next, .swiper-button-prev').css(
+								'top', $(swiper.$el).find('.thumb').outerHeight() * 0.5
+							)
+						})
+					}
+				}
+			}
+
+		articlesSliders.push(new Swiper('.articles_s' + i, options))
+
+		if (slides > articlesSliders[i].params.slidesPerView) {
+			options.loop = true
+			options.simulateTouch = true
+			options.allowTouchMove = true
+			options.noSwiping = false
+
+			articlesSliders[i].destroy(true, true)
+			articlesSliders[i] = new Swiper('.articles_s' + i, options)
+		}
+	})
+
+
+	// Товар - смена цвета
+	$('.product .colors .btn').click(function (e) {
+		e.preventDefault()
+
+		let btnIndex = $(this).index(),
+			parent = $(this).closest('.product')
+
+		parent.find('.colors .btn').removeClass('active')
+		$(this).addClass('active')
+
+		parent.find('.thumb img').hide()
+		parent.find('.thumb img').eq(btnIndex).fadeIn(200)
+	})
+
+
+	// Подменю
+	$('header .menu .item > a.sub_link').addClass('touch_link')
+
+	$('header .menu .item > a.sub_link').click(function (e) {
+		const $dropdown = $(this).next()
+
+		if ($dropdown.css('visibility') === 'hidden') {
+			e.preventDefault()
+
+			$('header .menu .sub_menu').removeClass('show')
+			$dropdown.addClass('show')
+		}
+	})
+
+	// Закрываем Подменю при клике за её пределами
+	$(document).click((e) => {
+		if ($(e.target).closest('.menu').length === 0) {
+			$('header .menu .sub_menu').removeClass('show')
+		}
+	})
+
+
+	// Корзина
+	$('header .btns .cart_btn, .mob_header .btns .cart_btn').click(function (e) {
+		e.preventDefault()
+
+		$('.auth_info').removeClass('show')
+		if ($(window).width() < 1024) { $('header').removeClass('show') }
+
+		$('body').addClass('menu_open')
+		$('.overlay').fadeIn(300)
+		$('.cart_info').removeClass('mob_hide').addClass('show')
+	})
+
+
+	// Авторизация
+	$('header .btns .auth_btn, .mob_header .btns .auth_btn').click(function (e) {
+		e.preventDefault()
+
+		$('.cart_info').removeClass('show')
+		if ($(window).width() < 1024) { $('header').removeClass('show') }
+
+		$('body').addClass('menu_open')
+		$('.overlay').fadeIn(300)
+		$('.auth_info.login').addClass('show')
+	})
+
+	// Авторизация - Регистрация
+	$('.auth_info .form .register_btn').click(function (e) {
+		e.preventDefault()
+
+		$('.auth_info.login').removeClass('show')
+
+		$('body').addClass('menu_open')
+		$('.overlay').fadeIn(300)
+		$('.auth_info.register').addClass('show')
+	})
+
+
+	// Моб. меню
+	$('.mob_header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('.auth_info, .cart_info').removeClass('show')
+
+		$('body').addClass('menu_open')
+		$('header').addClass('show')
+		$('.overlay').fadeIn(300)
+	})
+
+
+
+	$('.overlay').click((e) => {
+		e.preventDefault()
+
+		$('body').removeClass('menu_open')
+		$('.overlay').fadeOut(200)
+		$('.cart_info, .auth_info, header').removeClass('show')
+	})
+})
+
+
+
+$(window).on('resize', () => {
+	if (typeof WW !== 'undefined' && WW != $(window).width()) {
+		// Моб. версия
+		if (!fiestResize) {
+			$('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=1, maximum-scale=1')
+			if ($(window).width() < 375) $('meta[name=viewport]').attr('content', 'width=375, user-scalable=no')
+
+			fiestResize = true
+		} else {
+			fiestResize = false
+		}
+
+
+		// Перезапись ширины окна
+		WW = $(window).width()
+	}
+})
